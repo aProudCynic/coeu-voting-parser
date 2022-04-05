6e138a2d23ad4f28aa46eacd3233b8df3d684f76
Balazs Marton <balazs.marton@prezi.com>
2022-04-05 21:15:04 +0200

    feat: amend voting data with dates

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from os.path import exists
 # copied from query at:
 # https://www.consilium.europa.eu/en/general-secretariat/corporate-policies/transparency/open-data/voting-results/
 SPARQL_QUERY_FOR_ALL_VOTES = "PREFIX acts: <http://data.consilium.europa.eu/id/acts/>  PREFIX tax: <http://data.consilium.europa.eu/id/taxonomy/>  PREFIX codi: <http://data.consilium.europa.eu/def/codi/>  PREFIX skos:<http://www.w3.org/2004/02/skos/core#>  PREFIX dct: <http://purl.org/dc/terms/>  PREFIX foaf: <http://xmlns.com/foaf/0.1/>  PREFIX owl: <http://www.w3.org/2002/07/owl#>  PREFIX votpos: <http://data.consilium.europa.eu/id/taxonomy/votingposition>  SELECT distinct ?voteProc ?votingRuleId ?policyAreaId ?voteOnDocumentNumber ?votingInstCode ?legisProcId ?decisionDate  group_concat(distinct ?councilActionId;separator='|') as ?councilActionIdGrouped ?meetingSessionNumber ?councilConfigurationId  ?docExpression ?docTitle ?registerPage ?actNumber ?actTypeId  FROM <http://data.consilium.europa.eu/id/dataset/VotingResults>  FROM <http://data.consilium.europa.eu/id/dataset/PublicRegister>  WHERE {    ?voteProc a codi:VotingProcedure.      ?voteProc codi:votingRule ?votingRule.      ?votingRule skos:notation ?votingRuleId    optional {      ?voteProc codi:policyArea ?policyArea.      ?policyArea skos:notation ?policyAreaId    }.    optional {       ?voteProc codi:voteOn ?voteOn.      FILTER (CONTAINS(STR(?voteOn), 'INIT')).      ?voteOn codi:document_number ?voteOnDocumentNumber.      optional { ?voteOn codi:act_number ?actNumber. }.      optional {        ?voteOn codi:actType ?actType.        ?actType skos:notation ?actTypeId.      }.      optional { ?voteOn foaf:page ?registerPage. }      ?voteOn codi:expressed ?docExpression.      ?docExpression dct:title ?docTitle.      ?docExpression dct:language ?docLanguage.      FILTER ( lang(?docTitle) = 'en' || lang(?docTitle) = 'en' )    }.    optional { ?voteProc codi:forInterInstitutionalCode ?votingInstCode }.    optional {      ?voteProc codi:legislativeProcedure ?legisProc.      ?legisProc skos:notation ?legisProcId.    }.    ?voteProc codi:hasVoteOutcome ?voteDecision.    ?voteDecision dct:dateAccepted ?decisionDate.    optional {      ?voteDecision codi:councilAction ?councilAction.      ?councilAction skos:notation ?councilActionId.    }.    optional {      ?meeting codi:appliesProcedure ?voteProc.      optional { ?meeting codi:meetingsessionnumber ?meetingSessionNumber. }      optional {        ?meeting codi:configuration ?meetingConfig.        ?meetingConfig a skos:Concept.        ?meetingConfig skos:notation ?councilConfigurationId.      }    }  } ORDER BY DESC(?decisionDate), ?votingInstCode"
-SPARQL_QUERY_FOR_ALL_VOTE_RESULTS = "PREFIX acts: <http://data.consilium.europa.eu/id/acts/>  PREFIX tax: <http://data.consilium.europa.eu/id/taxonomy/>  PREFIX codi: <http://data.consilium.europa.eu/def/codi/>  PREFIX skos:<http://www.w3.org/2004/02/skos/core#>  PREFIX dct: <http://purl.org/dc/terms/>  PREFIX foaf: <http://xmlns.com/foaf/0.1/>  PREFIX owl: <http://www.w3.org/2002/07/owl#>  PREFIX votpos: <http://data.consilium.europa.eu/id/taxonomy/votingposition>  SELECT distinct ?voteProc ?voteDecision  group_concat(distinct ?countryCodeInFavour;separator='|') as ?countryCodeInFavourGrouped  group_concat(distinct ?countryCodeAgainst;separator='|') as ?countryCodeAgainstGrouped  group_concat(distinct ?countryCodeAbstained;separator='|') as ?countryCodeAbstainedGrouped  group_concat(distinct ?countryCodeNotParticipating;separator='|') as ?countryCodeNotParticipatingGrouped  FROM <http://data.consilium.europa.eu/id/dataset/VotingResults>  WHERE {     ?voteProc a codi:VotingProcedure.     ?voteProc codi:hasVoteOutcome ?voteDecision.     ?voteDecision codi:hasVotingPosition ?countryVote_uri .     optional {        ?countryVote_uri codi:votingposition <http://data.consilium.europa.eu/id/taxonomy/votingposition/votedagainst>.        ?countryVote_uri codi:country ?countryVoteAgainst_uri.        ?countryVoteAgainst_uri skos:notation ?countryCodeAgainst.     }     optional {        ?countryVote_uri codi:votingposition <http://data.consilium.europa.eu/id/taxonomy/votingposition/votedinfavour>.        ?countryVote_uri codi:country ?countryVoteInFavour_uri.        ?countryVoteInFavour_uri skos:notation ?countryCodeInFavour.     }     optional {        ?countryVote_uri codi:votingposition <http://data.consilium.europa.eu/id/taxonomy/votingposition/abstained>.        ?countryVote_uri codi:country ?countryAbstained_uri.        ?countryAbstained_uri skos:notation ?countryCodeAbstained.     }     optional {        ?countryVote_uri codi:votingposition <http://data.consilium.europa.eu/id/taxonomy/votingposition/notparticipating>.        ?countryVote_uri codi:country ?countryNotParticipating_uri.        ?countryNotParticipating_uri skos:notation ?countryCodeNotParticipating.     }  } "
+SPARQL_QUERY_FOR_ALL_VOTE_RESULTS = "PREFIX acts: <http://data.consilium.europa.eu/id/acts/>  PREFIX tax: <http://data.consilium.europa.eu/id/taxonomy/>  PREFIX codi: <http://data.consilium.europa.eu/def/codi/>  PREFIX skos:<http://www.w3.org/2004/02/skos/core#>  PREFIX dct: <http://purl.org/dc/terms/>  PREFIX foaf: <http://xmlns.com/foaf/0.1/>  PREFIX owl: <http://www.w3.org/2002/07/owl#>  PREFIX votpos: <http://data.consilium.europa.eu/id/taxonomy/votingposition>  SELECT distinct ?voteProc ?voteDecision ?decisionDate  group_concat(distinct ?countryCodeInFavour;separator='|') as ?countryCodeInFavourGrouped  group_concat(distinct ?countryCodeAgainst;separator='|') as ?countryCodeAgainstGrouped  group_concat(distinct ?countryCodeAbstained;separator='|') as ?countryCodeAbstainedGrouped  group_concat(distinct ?countryCodeNotParticipating;separator='|') as ?countryCodeNotParticipatingGrouped  FROM <http://data.consilium.europa.eu/id/dataset/VotingResults>  WHERE {     ?voteProc a codi:VotingProcedure.     ?voteProc codi:hasVoteOutcome ?voteDecision.     ?voteDecision codi:hasVotingPosition ?countryVote_uri .     optional {        ?countryVote_uri codi:votingposition <http://data.consilium.europa.eu/id/taxonomy/votingposition/votedagainst>.        ?countryVote_uri codi:country ?countryVoteAgainst_uri.        ?countryVoteAgainst_uri skos:notation ?countryCodeAgainst.     }     optional {        ?countryVote_uri codi:votingposition <http://data.consilium.europa.eu/id/taxonomy/votingposition/votedinfavour>.        ?countryVote_uri codi:country ?countryVoteInFavour_uri.        ?countryVoteInFavour_uri skos:notation ?countryCodeInFavour.     }     optional {        ?countryVote_uri codi:votingposition <http://data.consilium.europa.eu/id/taxonomy/votingposition/abstained>.        ?countryVote_uri codi:country ?countryAbstained_uri.        ?countryAbstained_uri skos:notation ?countryCodeAbstained.     }     optional {        ?countryVote_uri codi:votingposition <http://data.consilium.europa.eu/id/taxonomy/votingposition/notparticipating>.        ?countryVote_uri codi:country ?countryNotParticipating_uri.        ?countryNotParticipating_uri skos:notation ?countryCodeNotParticipating.     }  } "
 
 VOTE_CODE_YES = "Y"
 VOTE_CODE_NO = "N"
@@ -127,14 +127,32 @@ def calculate_group_vote_data(votes_by_member_states):
     return group_vote_data
 
 
+def merge_voting_data(votings, voting_results):
+    enriched_voting_data = []
+    for voting_result in voting_results:
+        vote_process_url = voting_result['voteProc']['value']
+        voting_data_results = [voting for voting in votings if voting['voteProc']['value'] == vote_process_url]
+        # TODO check sparql query to eliminate this part
+        assert len(
+            set([voting_data_result['decisionDate']['value'] for voting_data_result in voting_data_results])
+        ) == 1
+        voting_data = voting_data_results[0]
+        enriched_voting_data.append({**voting_result, 'decisionDate': voting_data['decisionDate']['value']})
+    return enriched_voting_data
+
+
 if __name__ == "__main__":
 
     voting_results = fetch_query_results(SPARQL_QUERY_FOR_ALL_VOTE_RESULTS)
 
+    votings = fetch_query_results(SPARQL_QUERY_FOR_ALL_VOTES)
+
+    full_voting_data = merge_voting_data(votings, voting_results)
+
     votes_by_member_states = get_cacheable_data(
         VOTES_BY_MEMBER_STATES_FILENAME,
         calculate_votes_by_member_states,
-        voting_results,
+        full_voting_data,
     )
 
     votings_together, same_votes = get_processed_vote_data(votes_by_member_states)
